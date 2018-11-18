a12b6028a3c64fb1a9b324691fd527cd120f7086
Peejay Garcia <peejgarcia@gmail.com>
2018-11-18 13:20:30 -0600

    Create Jess is Pretty!.js

---
+++ Jess is Pretty!.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your name?")
+if (firstName === "Jess" || firstName === "Jessica") 
+var lastName = prompt("What is your last name?")		
+if (firstName === "Jess" && lastName === "Effinger") 
+{alert("THE Jessica Effinger? You're the prettiest Lebanese on Highway K!")}
+	else { alert("You dusty!")}
